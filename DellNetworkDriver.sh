f4ca5fa0dc4781413a4f94243b7255c1d5f549ab
Amzad Mojumder <amzad.fof@gmail.com>
2014-04-13 00:38:57 +0100

    updated DellNetworkDriver.sh

--- DellNetworkDriver.sh
+++ DellNetworkDriver.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+# Run $ chmod +x DellNetworkDriver.sh
+# $ ./DellNetworkDriver.sh
+
+DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FILE_SCRIPT=$(basename $0);
+
+
+
+
+
+
+
+echo "FILE :: $DIR_SCRIPT/$FILE_SCRIPT";
+chmod 400 $DIR_SCRIPT/$FILE_SCRIPT;
