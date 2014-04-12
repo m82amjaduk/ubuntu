4fe5ab67a5745b19f7cc1231b0d123564609a92f
Amzad Mojumder <amzad.fof@gmail.com>
2014-04-12 23:58:56 +0100

    Updated Webmin Installation script

--- webminInstall.sh
+++ webminInstall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+DIR_SCRIPT=$(pwd);
+FILE_SCRIPT=$(basename $0);
+
+
 cd ~
 cd Packages
 
@@ -8,3 +12,8 @@ wget http://prdownloads.sourceforge.net/webadmin/webmin_1.580_all.deb
 sudo dpkg -i webmin_1.580_all.deb
 
 # https://127.0.0.1:10000
+
+
+cd $DIR_SCRIPT;
+msge "FILE :: $DIR_SCRIPT/$FILE_SCRIPT";
+chmod 400 $DIR_SCRIPT/$FILE_SCRIPT;
