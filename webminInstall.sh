4069ad5d7de18a12c8ab304f76c43c277b9792cc
Amzad Mojumder <amzad.fof@gmail.com>
2014-04-13 00:29:30 +0100

    Updated webminInstall.sh

--- webminInstall.sh
+++ webminInstall.sh
@@ -5,10 +5,9 @@ FILE_SCRIPT=$(basename $0);
 
 cd ~/Packages
 
-
-#sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
-#wget http://prdownloads.sourceforge.net/webadmin/webmin_1.580_all.deb
-#sudo dpkg -i webmin_1.580_all.deb
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.580_all.deb
+sudo dpkg -i webmin_1.580_all.deb
 
 # https://127.0.0.1:10000
 
