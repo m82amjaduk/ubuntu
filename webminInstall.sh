35f592ef938e5c49f690a3774787ac692eed523c
Amzad Mojumder <amzad.fof@gmail.com>
2014-04-13 00:28:20 +0100

    Updated webminInstall.sh

--- webminInstall.sh
+++ webminInstall.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-DIR_SCRIPT=$(pwd);
+DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FILE_SCRIPT=$(basename $0);
 
 
-cd ~
-cd Packages
+cd ~/Packages
 
 
-sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.580_all.deb
-sudo dpkg -i webmin_1.580_all.deb
+#sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+#wget http://prdownloads.sourceforge.net/webadmin/webmin_1.580_all.deb
+#sudo dpkg -i webmin_1.580_all.deb
 
 # https://127.0.0.1:10000
 
 
 cd $DIR_SCRIPT;
-msge "FILE :: $DIR_SCRIPT/$FILE_SCRIPT";
+echo "FILE :: $DIR_SCRIPT/$FILE_SCRIPT";
 chmod 400 $DIR_SCRIPT/$FILE_SCRIPT;
