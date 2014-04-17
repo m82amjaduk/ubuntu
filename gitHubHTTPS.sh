06edc0f685cb02684a91e51ab6e89cbf8f24722a
Amzad Mojumder <amzad.fof@gmail.com>
2014-04-17 15:59:05 +0100

    Create gitHubHTTPS.sh

---
+++ gitHubHTTPS.sh
@@ -0,0 +1,10 @@
+
+git init
+git remote add origin $ORIGIN_URL;
+git remote set-url origin $REMOTE_URL;
+git config  user.name "Amzad Mojumder" ;
+git config  user.email "amzadFOF@gmail.com";
+git config  credential.helper cache ;
+git config  credential.helper "cache --timeout=36000" ;
+
+git pull origin master	;
