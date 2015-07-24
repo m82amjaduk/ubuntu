fe45df99aaf45ca02255c5c3ff0413e08c3f36ab
Amzad Mojumder <amzadfof@gmail.com>
2015-07-24 19:56:28 +0100

    Create secureCentOS7.sh

---
+++ secureCentOS7.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# chmod +x /root/secureCentOS7.sh
+# /root/secureCentOS7.sh 22 amzad
+
+
+
+LOGDIR="/root/log/";
+LOGFILE=$LOGDIR"secure.log"
+mkdir -p $LOGDIR  /root/tmp/
+
+cd /root/tmp/
+
+SSH_PORT=$1     # '22'
+USER=$2         #'amzad'
+
+
+# Create User
+adduser $USER  &>> $LOGFILE;
+passwd $USER
+id $USER  &>> $LOGFILE;
+ls -lad /home/$USER/   &>> $LOGFILE;
+echo $USER ' ALL=(ALL) ALL' >> /etc/sudoers  # Do Not Insert If exist.
+
+
+# Modify sshd_config
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ORI  # Do not run if exist. /etc/issue.net
+vim /etc/ssh/sshd_config
+sed -i "s/#Port .*/Port 22/" /etc/ssh/sshd_config
+sed -i "s/#PermitRootLogin .*/PermitRootLogin no/" /etc/ssh/sshd_config
+sed -i "s/#PubkeyAuthentication .*/PubkeyAuthentication yes/" /etc/ssh/sshd_config
+
+
+sed -i "s/#Banner .*/Banner /etc/issue.net /" /etc/ssh/sshd_config  # Need to do this manually
+
+rm  /etc/issue.net
+echo "  ################################ " &>>  /etc/issue.net
+echo "  ConosurTek Ltd Server "  `hostname` &>>  /etc/issue.net
+echo "  ################################ " &>>  /etc/issue.net
+cat  /etc/issue.net  &>> $LOGFILE;
+
+/bin/systemctl restart  sshd.service  &>> $LOGFILE;
+# Access as both amzad and root !!
+#Banner none
