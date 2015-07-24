c103b7ccea8e0f03b30e29beade96979ed52875b
Amzad Mojumder <amzadfof@gmail.com>
2015-07-24 18:44:17 +0100

    Update newServer.sh

--- newServer.sh
+++ newServer.sh
@@ -2,13 +2,11 @@
 
 LOGDIR="/root/log/";
 LOGFILE=$LOGDIR"install.log"
-mkdir -p $LOGDIR
-
-mkdir -p /root/tmp/
+mkdir -p $LOGDIR /root/tmp/
 cd /root/tmp/
 
 function log() {
-    echo "\n Install " $1 " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " &>> $LOGFILE
+    echo   $1 " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " &>> $LOGFILE
 }
 
 function yumIns() {
@@ -19,9 +17,11 @@ function yumIns() {
 
 yumIns git;
 yumIns curl;
+yumIns vim;
+
 yumIns zsh;
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sed -i "s/ZSH_THEME=.*/ ZSH_THEME='nanotech'/" /root/.zshrc
+sed -i "s/ZSH_THEME=.*/ ZSH_THEME='nanotech'/" /root/.zshrc         # change theam
 
 wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 rpm -Uhv rpmforge-release*.rf.x86_64.rpm
